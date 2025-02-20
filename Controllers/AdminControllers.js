0b05ba5a1cfec0de8b68abfe3e14a35eca618e44
ksdkaveesha <ksdkaveesha@gmail.com>
2025-02-20 20:06:10 +0530

    Update AdminControllers.js

--- Controllers/AdminControllers.js
+++ Controllers/AdminControllers.js
@@ -23,3 +23,34 @@ exports.signUpUser = async (request, response) => {
     }
 };
 
+exports.signInUser = async (request, response) => {
+    const { email, password } = request.body;
+    try {
+        const signInUser = await Admin.findOne({ email });
+        if (!signInUser) {
+            return response.status(400).send({
+                status: 'error',
+                message: 'Invalid email or password'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, signInUser.password);
+        if (!isMatch) {
+            return response.status(400).send({
+                status: 'error',
+                message: 'Invalid password'
+            });
+        }
+
+        return response.status(200).send({
+            status: 'success',
+            user: signInUser
+        });
+    } catch (err) {
+        return response.status(400).send({
+            status: 'error',
+            message: err.message
+        });
+    }
+};
+
